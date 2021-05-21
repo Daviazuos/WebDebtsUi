7d7134981495d0a95a2bf78278a9d08d86d97334
Davi Souza <davi.souza@upperds.com>
2021-05-21 17:12:05 -0300

    Removendo inusados

--- src/components/table/table.jsx
+++ src/components/table/table.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from "react";
-import { Button, Table, Modal, Card } from "react-bootstrap";
+import React from "react";
+import { Button, Table } from "react-bootstrap";
 import axios from "axios";
 import "./Table.css";
 import ModalInstallments from "../installments/Installments"
